92c40489e79a399f2d12e02c58d901bb9eef3ef3
tyz8250 <tyz0308@gmail.com>
2026-01-04 18:33:31 +0900

    station5

--- handler/healthz.go
+++ handler/healthz.go
@@ -1,20 +1,23 @@
 package handler
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/TechBowl-japan/go-stations/model"
 )
 
-// A HealthzHandler implements health check endpoint.
 type HealthzHandler struct{}
 
-// NewHealthzHandler returns HealthzHandler based http.Handler.
-func NewHealthzHandler() *HealthzHandler {
-	return &HealthzHandler{}
-}
-
-// ServeHTTP implements http.Handler interface.
 func (h *HealthzHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	_ = &model.HealthzResponse{}
+	res := model.HealthzResponse{
+		Message: "OK",
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	if err := json.NewEncoder(w).Encode(res); err != nil {
+		log.Println(err)
+	}
 }
